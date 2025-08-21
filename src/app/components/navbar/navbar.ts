ce41b63143f8c0b8ecbc2210705499ec4242e5b5
Janeirungu01 <jwirungu96@gmail.com>
2025-08-21 11:59:38 +0300

    add a navbar

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,11 +1,36 @@
+// import { Component } from '@angular/core';
+// import { RouterOutlet, RouterLink } from '@angular/router';
+
+// @Component({
+//   selector: 'app-navbar',
+//   imports: [RouterLink, RouterOutlet],
+//   styleUrl: './navbar.css'
+//   template: `
+//     <nav>
+//       <a routerLink="/">Home</a> |
+//       <a routerLink="/cart">Cart</a>
+//     </nav>
+//     <router-outlet></router-outlet>
+//   `
+// })
+// export class Navbar {
+
+// }
+
 import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  imports: [],
-  templateUrl: './navbar.html',
-  styleUrl: './navbar.css'
+  standalone: true,
+  imports: [RouterOutlet, RouterLink],
+  template: `
+    <nav>
+      <a routerLink="/">Home</a> |
+      <a routerLink="/cart">Cart</a>
+    </nav>
+    <router-outlet></router-outlet>
+  `
 })
-export class Navbar {
-
-}
+export class Navbar {}
