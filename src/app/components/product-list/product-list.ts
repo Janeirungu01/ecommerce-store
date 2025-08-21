eed01a47d0120af264b6e3d5848571f0338f948d
Janeirungu01 <jwirungu96@gmail.com>
2025-08-21 09:46:10 +0300

    add product list component

---
+++ src/app/components/product-list/product-list.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { PRODUCTS } from '../../services/products';
+import { CartService } from '../../services/cart';
+
+@Component({
+  selector: 'app-product-list',
+  templateUrl: './product-list.html',
+})
+export class ProductListComponent {
+  products = PRODUCTS;
+
+  constructor(private cartService: CartService) {}
+
+  addToCart(product: any) {
+    this.cartService.addToCart(product);
+    alert(`${product.name} added to cart!`);
+  }
+}
+
---
+++ src/app/services/products.ts
@@ -0,0 +1,8 @@
+import { Product } from '../models/product.model';
+
+export const PRODUCTS: Product[] = [
+  { id: 1, name: 'Laptop', price: 75000, image: 'assets/laptop.jpg', description: 'Fast and reliable.' },
+  { id: 2, name: 'Smartphone', price: 25000, image: 'assets/phone.jpg', description: 'Latest model smartphone.' },
+  { id: 3, name: 'Headphones', price: 5000, image: 'assets/headphones.jpg', description: 'Noise cancelling.' }
+];
+
