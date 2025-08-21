39d6d9750d6779da225c65431afb013f3ac79e06
Janeirungu01 <jwirungu96@gmail.com>
2025-08-21 17:15:54 +0300

    add navbar with bootstrap

--- src/app/components/navbar/navbar.ts
+++ src/app/components/navbar/navbar.ts
@@ -1,36 +1,10 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet, RouterLink } from '@angular/router';
-
-// @Component({
-//   selector: 'app-navbar',
-//   imports: [RouterLink, RouterOutlet],
-//   styleUrl: './navbar.css'
-//   template: `
-//     <nav>
-//       <a routerLink="/">Home</a> |
-//       <a routerLink="/cart">Cart</a>
-//     </nav>
-//     <router-outlet></router-outlet>
-//   `
-// })
-// export class Navbar {
-
-// }
-
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
-  standalone: true,
   imports: [RouterOutlet, RouterLink],
-  template: `
-    <nav>
-      <a routerLink="/">Home</a> |
-      <a routerLink="/cart">Cart</a>
-    </nav>
-    <router-outlet></router-outlet>
-  `
+  templateUrl: './navbar.html'
 })
 export class Navbar {}
